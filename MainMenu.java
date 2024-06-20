2423dfd478d868eba43be26a431897d4993c38c8
Yen Huynh <90292203+YenHuynh02@users.noreply.github.com>
2024-06-19 23:09:17 -0400

    Updated MainMenu
    Co-authored-by: Yen Huynh <peterhuynh.contact@gmail.com>

--- MainMenu.java
+++ MainMenu.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class MainMenu extends JMenuBar {
     
     public MainMenu(JFrame frame) {
