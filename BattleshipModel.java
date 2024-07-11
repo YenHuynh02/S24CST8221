bc0f6172fe2af22e7e4f3d1976ab2e7cf4747d84
Yen Huynh <peterhuynh.contact@gmail.com>
2024-07-11 08:05:01 -0400

    Update BattleshipModel.java

--- BattleshipModel.java
+++ BattleshipModel.java
@@ -7,9 +7,7 @@ public class BattleshipModel {
     private boolean gameOver;
     private int playerHits;
     private int computerHits;
-    private int lastComputerAttackX;
-    private int lastComputerAttackY;
-    private boolean lastComputerAttackResult;
+    public final int SHIP_SIZE = 5; // Size of the ship
 
     public BattleshipModel() {
         playerBoard = new char[10][10];
@@ -19,7 +17,7 @@ public class BattleshipModel {
         playerHits = 0;
         computerHits = 0;
 
-        // Initialize boards
+        // Initialize boards with empty characters
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 playerBoard[i][j] = ' ';
@@ -27,127 +25,8 @@ public class BattleshipModel {
             }
         }
 
-        // Place ships on computer board randomly
-        placeComputerShips();
-    }
-
-    public void placePlayerShip(int x, int y, char orientation, int length) {
-        if (orientation == 'H') {
-            for (int i = 0; i < length; i++) {
-                playerBoard[x][y + i] = 'S';
-            }
-        } else {
-            for (int i = 0; i < length; i++) {
-                playerBoard[x + i][y] = 'S';
-            }
-        }
-    }
-
-    private void placeComputerShips() {
-        Random random = new Random();
-        int[] shipLengths = {5, 4, 3, 3, 2};
-
-        for (int length : shipLengths) {
-            boolean placed = false;
-
-            while (!placed) {
-                int x = random.nextInt(10);
-                int y = random.nextInt(10);
-                char orientation = random.nextBoolean() ? 'H' : 'V';
-
-                if (canPlaceShip(computerBoard, x, y, orientation, length)) {
-                    placeShip(computerBoard, x, y, orientation, length);
-                    placed = true;
-                }
-            }
-        }
-    }
-
-    private boolean canPlaceShip(char[][] board, int x, int y, char orientation, int length) {
-        if (orientation == 'H') {
-            if (y + length > 10) return false;
-            for (int i = 0; i < length; i++) {
-                if (board[x][y + i] == 'S') return false;
-            }
-        } else {
-            if (x + length > 10) return false;
-            for (int i = 0; i < length; i++) {
-                if (board[x + i][y] == 'S') return false;
-            }
-        }
-        return true;
-    }
-
-    private void placeShip(char[][] board, int x, int y, char orientation, int length) {
-        if (orientation == 'H') {
-            for (int i = 0; i < length; i++) {
-                board[x][y + i] = 'S';
-            }
-        } else {
-            for (int i = 0; i < length; i++) {
-                board[x + i][y] = 'S';
-            }
-        }
-    }
-
-    public void playerAttack(int x, int y) {
-        if (computerBoard[x][y] == 'S') {
-            computerBoard[x][y] = 'H';
-            playerHits++;
-        } else if (computerBoard[x][y] == ' ') {
-            computerBoard[x][y] = 'M';
-        }
-        playerTurn = false;
-        checkGameOver();
-    }
-
-    public void computerAttack() {
-        Random random = new Random();
-        int x, y;
-        do {
-            x = random.nextInt(10);
-            y = random.nextInt(10);
-        } while (playerBoard[x][y] == 'M' || playerBoard[x][y] == 'H');
-
-        if (playerBoard[x][y] == 'S') {
-            playerBoard[x][y] = 'H';
-            computerHits++;
-            lastComputerAttackResult = true;
-        } else {
-            playerBoard[x][y] = 'M';
-            lastComputerAttackResult = false;
-        }
-
-        lastComputerAttackX = x;
-        lastComputerAttackY = y;
-
-        playerTurn = true;
-        checkGameOver();
-    }
-
-    private void checkGameOver() {
-        if (playerHits == 5) {
-            gameOver = true;
-        } else if (computerHits == 5) {
-            gameOver = true;
-        }
-    }
-
-    public boolean isPlayerTurn() {
-        return playerTurn;
-    }
-
-    public boolean isGameOver() {
-        return gameOver;
-    }
-
-    public String getWinner() {
-        if (playerHits == 5) {
-            return "Player";
-        } else if (computerHits == 5) {
-            return "Computer";
-        }
-        return "None";
+        // Randomly place computer ship
+        placeComputerShip();
     }
 
     public char[][] getPlayerBoard() {
@@ -158,19 +37,142 @@ public class BattleshipModel {
         return computerBoard;
     }
 
-    public int getLastComputerAttackX() {
-        return lastComputerAttackX;
+    public boolean isPlayerTurn() {
+        return playerTurn;
     }
 
-    public int getLastComputerAttackY() {
-        return lastComputerAttackY;
+    public boolean isGameOver() {
+        return gameOver;
     }
 
-    public boolean getLastComputerAttackResult() {
-        return lastComputerAttackResult;
+    public boolean playerAttack(int x, int y) {
+        boolean hit = false;
+        if (computerBoard[x][y] == ' ') {
+            computerBoard[x][y] = 'M';
+        } else if (computerBoard[x][y] == 'S') {
+            computerBoard[x][y] = 'H';
+            playerHits++;
+            hit = true;
+        }
+        playerTurn = false;
+        checkGameOver();
+        return hit;
     }
 
-    public void setPlayerTurn(boolean playerTurn) {
-        this.playerTurn = playerTurn;
+    public int[] computerAttack() {
+        int x, y;
+        Random rand = new Random();
+        do {
+            x = rand.nextInt(10);
+            y = rand.nextInt(10);
+        } while (playerBoard[x][y] == 'M' || playerBoard[x][y] == 'H');
+
+        if (playerBoard[x][y] == ' ') {
+            playerBoard[x][y] = 'M';
+        } else if (playerBoard[x][y] == 'S') {
+            playerBoard[x][y] = 'H';
+            computerHits++;
+        }
+        playerTurn = true;
+        checkGameOver();
+        return new int[]{x, y};
+    }
+
+    public void placePlayerShip(int x, int y, char orientation) {
+        if (orientation == 'H') {
+            for (int i = 0; i < SHIP_SIZE; i++) {
+                playerBoard[x][y + i] = 'S';
+            }
+        } else {
+            for (int i = 0; i < SHIP_SIZE; i++) {
+                playerBoard[x + i][y] = 'S';
+            }
+        }
+    }
+
+    private void placeComputerShip() {
+        Random rand = new Random();
+        boolean placed = false;
+        while (!placed) {
+            int x = rand.nextInt(10);
+            int y = rand.nextInt(10);
+            char orientation = rand.nextBoolean() ? 'H' : 'V';
+            if (canPlaceShip(computerBoard, x, y, orientation)) {
+                placeShip(computerBoard, x, y, orientation);
+                placed = true;
+            }
+        }
+    }
+
+    public boolean canPlaceShip(char[][] board, int x, int y, char orientation) {
+        if (orientation == 'H') {
+            if (y + SHIP_SIZE > 10) return false;
+            for (int i = 0; i < SHIP_SIZE; i++) {
+                if (board[x][y + i] != ' ') return false;
+            }
+        } else {
+            if (x + SHIP_SIZE > 10) return false;
+            for (int i = 0; i < SHIP_SIZE; i++) {
+                if (board[x + i][y] != ' ') return false;
+            }
+        }
+        return true;
+    }
+
+    private void placeShip(char[][] board, int x, int y, char orientation) {
+        if (orientation == 'H') {
+            for (int i = 0; i < SHIP_SIZE; i++) {
+                board[x][y + i] = 'S';
+            }
+        } else {
+            for (int i = 0; i < SHIP_SIZE; i++) {
+                board[x + i][y] = 'S';
+            }
+        }
+    }
+
+    private void checkGameOver() {
+        if (playerHits >= SHIP_SIZE) {
+            gameOver = true;
+        } else if (computerHits >= SHIP_SIZE) {
+            gameOver = true;
+        }
+    }
+
+    public String getWinner() {
+        if (playerHits >= SHIP_SIZE) {
+            return "Player";
+        } else if (computerHits >= SHIP_SIZE) {
+            return "Computer";
+        }
+        return null;
+    }
+
+    public int getPlayerHits() {
+        return playerHits;
+    }
+
+    public int getComputerHits() {
+        return computerHits;
+    }
+
+    public void resetGame() {
+        playerBoard = new char[10][10];
+        computerBoard = new char[10][10];
+        playerTurn = true;
+        gameOver = false;
+        playerHits = 0;
+        computerHits = 0;
+
+        // Initialize boards with empty characters
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                playerBoard[i][j] = ' ';
+                computerBoard[i][j] = ' ';
+            }
+        }
+
+        // Randomly place computer ship
+        placeComputerShip();
     }
 }
