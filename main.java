31750207a83c2d5dd4e6403159418cbedc9e1f57
Yen Huynh <peterhuynh.contact@gmail.com>
2024-07-11 08:06:43 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,22 @@
+import javax.swing.JFrame;
+
 public class main {
     public static void main(String[] args) {
-        BattleshipModel model = new BattleshipModel();
+        JFrame frame = new JFrame("Battleship");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
         BattleshipView view = new BattleshipView();
-        BattleshipController controller = new BattleshipController(model, view);
+        BattleshipModel model = new BattleshipModel();
+        new BattleshipController(view, model);
+
+        // Create and set the main menu
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.addMenuToFrame(frame);
+
+        frame.add(view.getMainPanel());  // Add the main panel from BattleshipView
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
